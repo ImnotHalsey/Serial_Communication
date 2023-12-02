551dd3db618254eb6434ac5a0c1f32525de5edbc
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-02 13:26:47 +0530

    Updated to list and added receiver code
    //APP as a receiver , Just codes will be printed!

--- bot_app_seriallData_sender/bot_app_seriallData_sender.ino
+++ bot_app_seriallData_sender/bot_app_seriallData_sender.ino
@@ -5,9 +5,11 @@ SoftwareSerial mySerial(10, 11);
 
 struct SensorData {
     long timestamp;
-    int botBattery;
+    float botBattery;
     int t1MotorTemperature;
-    int waterLevel;
+    int t2MotorTemperature;
+    int t3MotorDriverTemperature;
+    float waterLevel;
     int EmergencyAlert;
 };
 
@@ -21,6 +23,8 @@ SensorData generateRandomSensorData() {
     data.timestamp = 0;
     data.botBattery = random(45, 61);
     data.t1MotorTemperature = random(15, 121);
+    data.t2MotorTemperature = random(15, 121);
+    data.t3MotorDriverTemperature = random(15, 121);
     data.waterLevel = random(0, 101);
     data.EmergencyAlert = random(0, 4);
     return data;
@@ -36,3 +40,57 @@ void loop() {
     sendData(randomData);
     delay(1000);
 }
+
+//APP as a receiver , Just codes will be printed!
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11);
+
+struct SensorData {
+    long timestamp;
+    float botBattery;
+    int t1MotorTemperature;
+    int t2MotorTemperature;
+    int t3MotorDriverTemperature;
+    float waterLevel;
+    int EmergencyAlert;
+};
+
+void setup() {
+    Serial.begin(9600);
+    mySerial.begin(9600);
+}
+
+void receiveData(SensorData& data) {
+    // Read data as an array of individual values
+    if (mySerial.available() >= sizeof(SensorData)) {
+        mySerial.readBytes((char*)&data, sizeof(SensorData));
+    }
+}
+
+void printReceivedData(const SensorData& data) {
+    Serial.print("[");
+    Serial.print(data.timestamp);
+    Serial.print(",");
+    Serial.print(data.botBattery);
+    Serial.print(",");
+    Serial.print(data.t1MotorTemperature);
+    Serial.print(",");
+    Serial.print(data.t2MotorTemperature);
+    Serial.print(",");
+    Serial.print(data.t3MotorDriverTemperature);
+    Serial.print(",");
+    Serial.print(data.waterLevel);
+    Serial.print(",");
+    Serial.print(data.EmergencyAlert);
+    Serial.println("]");
+}
+
+void loop() {
+    SensorData receivedData;
+    receiveData(receivedData);
+    printReceivedData(receivedData);
+    delay(1000);
+}
+

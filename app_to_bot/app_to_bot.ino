3f80810373f76b8d2b38433d74d5e6013a262f89
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-06 12:16:48 +0530

    Optimized Sender Code
    ..Init..

--- app_to_bot/app_to_bot.ino
+++ app_to_bot/app_to_bot.ino
@@ -1,16 +1,6 @@
-//  Bot To APP --> Mode 50- 100ms communication 
+// Bot To APP --> Mode 50-100ms communication
 #include <SoftwareSerial.h>
-
 SoftwareSerial mySerial(10, 11);
-struct SensorData {
-    long timestamp;
-    int Message_ID;
-    int Control_mode;
-    int path_mode;
-    int lights;
-    int Emergency;
-    int tool_type;
-};
 
 void setup() {
     Serial.begin(9600);
@@ -20,63 +10,53 @@ void setup() {
 // Class for all communication processing ...
 class SensorDataProcessor {
 public:
-  long get_timestamp() { return 0; }
-  int get_Message_ID() { return random(1001, 1099);}
-  int get_Control_mode() { return random(0, 3);}
-  int get_path_mode() { return random(0, 2);}
-  int get_lights() { return random(0, 2);}
-  int get_Emergency() { return random(0, 2);}
-  int tool_type() { return random(0, 7);}
+    long get_timestamp() { return 0; }
+    int get_Message_ID() { return random(1001, 1099); }
+    int get_Control_mode() { return random(0, 3); }
+    int get_path_mode() { return random(0, 2); }
+    int get_lights() { return random(0, 2); }
+    int get_Emergency() { return random(0, 2); }
+    int get_tool_type() { return random(0, 7); }
 };
 
-
-SensorData ArrayMaker() {
+int* ArrayMaker() {
+    static int results[7] = {0, 0, 0, 0, 0, 0, 0};
     SensorDataProcessor dataProcessor;
-    SensorData data;
 
-    data.timestamp = dataProcessor.get_timestamp();
-    data.Message_ID = dataProcessor.get_Message_ID();
-    data.Control_mode = dataProcessor.get_Control_mode();
-    data.path_mode = dataProcessor.get_path_mode();
-    data.lights = dataProcessor.get_lights();
-    data.Emergency = dataProcessor.get_Emergency();
-    data.tool_type = dataProcessor.tool_type();
+    // Store results in the array
+    results[0] = dataProcessor.get_timestamp();
+    results[1] = dataProcessor.get_Message_ID();
+    results[2] = dataProcessor.get_Control_mode();
+    results[3] = dataProcessor.get_path_mode();
+    results[4] = dataProcessor.get_lights();
+    results[5] = dataProcessor.get_Emergency();
+    results[6] = dataProcessor.get_tool_type();
 
-    return data;
+    return results;
 }
 
+void printer() {
+    int* results = ArrayMaker();
 
-void sendData(const SensorData & data) {
-    mySerial.write((uint8_t*)&data, sizeof(SensorData));
-
-    // Print individual values without a custom print function --> Remove this in prod !
-    Serial.println("Sent data as an array");
-    Serial.print("get_TimeStamp: "); Serial.println(data.timestamp);
-    Serial.print("get_Message_ID: "); Serial.println(data.Message_ID);
-    Serial.print("get_Control_mode: "); Serial.println(data.Control_mode);
-    Serial.print("get_path_mode: "); Serial.println(data.path_mode);
-    Serial.print("get_lights: "); Serial.println(data.lights);
-    Serial.print("get_Emergency: "); Serial.println(data.Emergency);
-    Serial.print("tool_type: "); Serial.println(data.tool_type);
-
+    // Print results from the array
+    Serial.print("Timestamp: ");
+    Serial.println(results[0]);
+    Serial.print("Message ID: ");
+    Serial.println(results[1]);
+    Serial.print("Control Mode: ");
+    Serial.println(results[2]);
+    Serial.print("Path Mode: ");
+    Serial.println(results[3]);
+    Serial.print("Lights: ");
+    Serial.println(results[4]);
+    Serial.print("Emergency: ");
+    Serial.println(results[5]);
+    Serial.print("Tool Type: ");
+    Serial.println(results[6]);
+    Serial.println();
 }
 
 void loop() {
-    SensorData randomData = ArrayMaker();
-    sendData(randomData);
+    printer();
     delay(1000);
 }
-                    // Sending code End!
-
-                    //  -- > Base code version :: 1
-
-                    //  --> Expected printed output 
-                    
-                              // -> Sent data as an array
-                              // -> get_TimeStamp: 0
-                              // -> get_Message_ID: 1037
-                              // -> get_Control_mode: 0
-                              // -> get_path_mode: 1
-                              // -> get_lights: 0
-                              // -> get_Emergency: 0
-                              // -> tool_type: 2

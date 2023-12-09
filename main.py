84b247c3a3b69f9c4088d263c8ea0b2de8a9f493
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-09 23:06:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ bot = telebot.TeleBot('6900589010:AAFR1daWHcRsdc8h025DrEpyk6eVN2ePUHc')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    print("Bot replied")
     bot.reply_to(message, "Welcome! I am your bot.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
+print("The bot activated")
 bot.polling()

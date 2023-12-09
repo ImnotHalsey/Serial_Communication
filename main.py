e9f3c03d89fddee14a8fb7a660ec8f7a3771aa63
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-09 21:53:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import time
+from datetime import datetime
+import pytz
 
-while 1:
-    print("Hesinberg here")
-    time.sleep(2.5)
+# Set the time zone to Indian Standard Time
+india_timezone = pytz.timezone('Asia/Kolkata')
+
+# Get the current time in UTC
+utc_now = datetime.utcnow()
+
+# Convert the UTC time to Indian Standard Time
+ist_now = utc_now.replace(tzinfo=pytz.utc).astimezone(india_timezone)
+
+# Format and print the time
+formatted_time = ist_now.strftime('%Y-%m-%d %H:%M:%S %Z')
+print(f"Current time in India (IST): {formatted_time}")
